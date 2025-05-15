495a62132378ef31344410c12ec64cb86c1a2a47
Tumwesigenelson-coder <chatwithnelly94@gmail.com>
2025-05-15 10:27:10 +0300

    Create Program.cs

---
+++ week02/Program.cs
@@ -0,0 +1,36 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World! This is the week 02 Journal Project.");
+
+        // Creating first job
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2019;
+        job1._endYear = 2022;
+
+        // Creating second job
+        Job job2 = new Job();
+        job2._jobTitle = "Manager";
+        job2._company = "Apple";
+        job2._startYear = 2022;
+        job2._endYear = 2023;
+
+        // Creating resume
+        Resume myResume = new Resume();
+        myResume._name = "Tumwesige Nelson";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        // Displaying resume
+        myResume.Display();
+
+        Console.WriteLine();
+    }
+    
+    
+}
