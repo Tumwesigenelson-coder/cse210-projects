6fb04d05daf6739262707f6df4c82dc05eb63f49
Tumwesigenelson-coder <chatwithnelly94@gmail.com>
2025-06-17 11:17:28 +0300

    week 7 assignment

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+        Console.WriteLine("Hello World! This is my ExerciseTracking Project.");
+
+         List<Activity> activities = new List<Activity>
+        {
+            new Running("15 Feb 2022", 30, 4.8),
+            new Cycling("15 Feb 2022", 45, 15.0),
+            new Swimming("15 Feb 2022", 40, 60)
+        };
+
+        foreach (Activity activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
